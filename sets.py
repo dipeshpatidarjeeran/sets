9e623975ed6811bca703fac27262573c70ce0a28
dipeshpatidarjeeran <dipeshjeeran@gmail.com>
2021-11-27 21:22:41 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,18 @@
+#{} = set
+
+class1 = {"rohit","ajay","dipesh","dipak"}
+
+print(class1)
+
+class2 = {'rajesh',"dilip","yogesh"}
+
+print(class2) 
+
+print(type(class1))
+#set.clear()
+class1.clear()                       #removing all the eliment
+print(len(class1))                   #leight of sets 
+
+print(class1)
+class1 ={"a",'b',"c"}
+print(class1)
